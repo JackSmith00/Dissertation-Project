0d0853963ecb30abd10431aa4a26d97a60964b3b
Jack-24265241 <24265241@edgehill.ac.uk>
2023-02-16 16:11:29 +0000

    Commenting
    Commented code complete so far

--- web_scraper/bbc_web_scraper.py
+++ web_scraper/bbc_web_scraper.py
@@ -1,48 +1,67 @@
-from cgitb import text
-
 import requests
 from bs4 import BeautifulSoup
 
 
 def extract_page(url: str) -> BeautifulSoup:
-    # get html content
-    page = requests.get(url)
-    return BeautifulSoup(page.content, "html.parser")
+    """Retrieves the HTML of a web page and
+    returns it as a BeautifulSoup object
+
+    :param url: The url address of the webpage
+    :returns: A BeautifulSoup object representing the website HTML
+    """
+    page = requests.get(url)  # get html content
+    return BeautifulSoup(page.content, "html.parser")  # return it as a BeautifulSoup object
 
 
 def isolate_bbc_text(soup: BeautifulSoup) -> [str]:
-    article = soup.find("article")
-    article_text = [article.h1.get_text().strip()]
-    text_blocks = [
+    """Takes the HTML of a BBC article as a BeautifulSoup object and
+    extracts the relevant information: title, headings and paragraphs.
+    This is returned as a list of Strings, starting with the title and
+    each following entry being a heading or paragraph
+    :param soup: A BeautifulSoup object containing the HTML of a BBC article
+    :returns: A list containing each line of relevant text in the article"""
+    article = soup.find("article")  # isolate article
+    text = [article.h1.get_text().strip()]  # store article header as 1st line
+
+    text_blocks = [  # retrieve all blocks containing text or headings
         div for div in article.find_all("div")
         if div.attrs.get("data-component") == "text-block" or div.attrs.get("data-component") == "subheadline-block"
     ]
-    for div in text_blocks:
-        for p in div.find_all(["p", "h2"]):  # https://www.projectpro.io/recipes/pass-attributes-in-find-and-find-all-functions-of-beautiful-soup
-            if p.get_text() != "":
-                article_text.append(p.get_text().strip())
+    # this method prevents irrelevant paragraphs being captured, such as related
+    # article snippets and video player error warnings
 
-    return article_text
+    for div in text_blocks:
+        for p in div.find_all(["p", "h2"]):  # seperate each paragraph and heading
+            """
+            The passing of multiple arguments to find_all() was researched at the following resource:
+            
+            PROJECTPRO, 2022. How to pass attributes in the find functions of beautiful Soup [online].
+            Available from: https://www.projectpro.io/recipes/pass-attributes-in-find-and-find-all-functions-of-beautiful-soup
+            [Accessed 16 February 2023]
+            
+            The code was not copied verbatim; it was simply used as a guide how to better use the find_all() function.
+            """
+            if p.get_text() != "":  # ignore blank lines
+                text.append(p.get_text().strip())  # add current line to the end of the current list
+
+    return text  # return all article text
 
 
 def save_to_corpus(lines: [str], save_location: str):
-    with open(save_location, "w") as f:
+    """Saves given lines of text to a specified location
+    :param lines: the text to write to the file
+    :param save_location: the path to which to save the file"""
+    with open(save_location, "w") as f:  # will automatically close file
         for line in lines:
+            # write each line on a new line in the file
             f.write(line + "\n")
 
 
+def retrieve_sitemap_data(sitemap_url: str):
+    pass
+    page = requests.get(sitemap_url)
+    sitemap_xml = BeautifulSoup(page.content, "xml.parser")
+
+
 if __name__ == '__main__':
-    with open("/Users/Jack/Documents/UNI/Project/Code/Example Soup/world-europe-64626783.html", "r") as file:
-        a1 = BeautifulSoup(file.read(), "html.parser")
-    with open("/Users/Jack/Documents/UNI/Project/Code/Example Soup/cw5pvgn7356o.html", "r") as file:
-        a2 = BeautifulSoup(file.read(), "html.parser")
-    with open("/Users/Jack/Documents/UNI/Project/Code/Example Soup/uk-england-12457929.html", 'r') as file:
-        a3 = BeautifulSoup(file.read(), "html.parser")
-
-    new_page = extract_page("https://www.bbc.co.uk/news/education-63283289")
-    page_text = isolate_bbc_text(new_page)
-    save_to_corpus(page_text, "/Users/Jack/Documents/UNI/Project/Code/Example Soup/education-63283289.txt")
-
-    isolate_bbc_text(a1)
-    isolate_bbc_text(a2)
-    isolate_bbc_text(a3)
+    pass
