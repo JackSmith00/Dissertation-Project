9c610fb08960d0fcd6c5277a352073275dc3fed6
Jack-24265241 <24265241@edgehill.ac.uk>
2023-04-10 20:00:02 +0100

    Update supervised_sa.py
    Renamed appropriately.
    
    Moved logic into functions.
    
    Made use of pickle module to save classifiers so that they do not need to be trained every time the program is ran.
    
    Tested the classification works.

--- sentiment_analysis/supervised_sa.py
+++ sentiment_analysis/supervised_sa.py
@@ -21,6 +21,7 @@ import os.path
 
 vader_analyser = SentimentIntensityAnalyzer()
 flair_analyser = TextClassifier.load("en-sentiment")
+corpora_regex = "[a-zA-Z0-9-_]*.txt"
 
 biased_words = []
 manually_extracted_biased_words = []
@@ -33,10 +34,12 @@ non_biased_bigrams = []
 subjective_bigrams = []
 objective_bigrams = []
 
+labeled_dataset_features = []
+
 
 def load_feature(feature: list, location: str):
-    with open(location) as f:
-        for line in f.readlines():
+    with open(location) as file:
+        for line in file.readlines():
             feature.append(line.strip())
 
 
@@ -117,65 +120,111 @@ def extract_features(text: str) -> dict:
     return extracted_features
 
 
-if __name__ == '__main__':
-
-    corpora_regex = "[a-zA-Z0-9-_]*.txt"
-
-    # read feature word lists
-    #load_all_features()
-
-    # get features for training
+def get_labeled_features():
     if os.path.exists("/Volumes/24265241/Unsupervised Training/labeled_dataset_features.pkl"):
-        with open("/Volumes/24265241/Unsupervised Training/labeled_dataset_features.pkl", "rb") as f:
-            labeled_dataset_features = pickle.load(f)
+        with open("/Volumes/24265241/Unsupervised Training/labeled_dataset_features.pkl", "rb") as file:
+            labeled_dataset_features.extend(pickle.load(file))
     else:
         biased_articles = PlaintextCorpusReader("/Volumes/24265241/Unsupervised Training/Biased", corpora_regex)
         non_biased_articles = PlaintextCorpusReader("/Volumes/24265241/Unsupervised Training/Non-biased", corpora_regex)
 
-        labeled_dataset_features = [
+        labeled_dataset_features.extend([
             (extract_features(biased_articles.raw(article)), 'biased')
             for article in tqdm(biased_articles.fileids(), desc="Biased feature extraction")
-        ]
+        ])
 
         labeled_dataset_features.extend([
             (extract_features(non_biased_articles.raw(article)), 'not biased')
             for article in tqdm(non_biased_articles.fileids(), desc="Non-biased feature extraction")
         ])
 
-        with open("/Volumes/24265241/Unsupervised Training/labeled_dataset_features.pkl", "wb") as f:
-            pickle.dump(labeled_dataset_features, f)
+        with open("/Volumes/24265241/Unsupervised Training/labeled_dataset_features.pkl", "wb") as file:
+            pickle.dump(labeled_dataset_features, file)
 
-    # train various classifiers
+
+def test_classifiers() -> dict[str, float]:
     train_count = (len(labeled_dataset_features) // 4) * 3  # train with 75% of the dataset
 
-    for i in range(1, 11):
-        shuffle(labeled_dataset_features)
-        model_accuracies = dict()
+    shuffle(labeled_dataset_features)
+    model_accuracies = dict()
 
-        # handle naive bayes separately as from nltk not sklearn
-        naive_bayes = NaiveBayesClassifier.train(labeled_dataset_features[:train_count])
-        model_accuracies["Naive Bayes"] = accuracy(naive_bayes, labeled_dataset_features[train_count:])
+    # handle naive bayes separately as from nltk not sklearn
+    naive_bayes = NaiveBayesClassifier.train(labeled_dataset_features[:train_count])
+    model_accuracies["Naive Bayes"] = accuracy(naive_bayes, labeled_dataset_features[train_count:])
 
-        # handle remaining classifiers
-        sklearn_classifiers = {
-            "BernoulliNB": BernoulliNB(),
-            "ComplementNB": ComplementNB(),
-            "MultinomialNB": MultinomialNB(),
-            "KNeighborsClassifier": KNeighborsClassifier(),
-            "DecisionTreeClassifier": DecisionTreeClassifier(),
-            "RandomForestClassifier": RandomForestClassifier(),
-            "LogisticRegression": LogisticRegression(max_iter=1500),
-            "MLPClassifier": MLPClassifier(max_iter=1500),
-            "AdaBoostClassifier": AdaBoostClassifier(),
+    # handle remaining classifiers
+    sklearn_classifiers = {
+        "BernoulliNB": BernoulliNB(),
+        "ComplementNB": ComplementNB(),
+        "MultinomialNB": MultinomialNB(),
+        "KNeighborsClassifier": KNeighborsClassifier(),
+        "DecisionTreeClassifier": DecisionTreeClassifier(),
+        "RandomForestClassifier": RandomForestClassifier(),
+        "LogisticRegression": LogisticRegression(max_iter=1500),
+        "MLPClassifier": MLPClassifier(max_iter=1500),
+        "AdaBoostClassifier": AdaBoostClassifier(),
+    }
+
+    for model_name, model in tqdm(sklearn_classifiers.items(), desc="Testing Models"):
+        classifier = SklearnClassifier(model)
+        classifier.train(labeled_dataset_features[:train_count])
+        model_accuracies[model_name] = accuracy(classifier, labeled_dataset_features[train_count:])
+
+    return model_accuracies
+
+
+if __name__ == '__main__':
+    # read feature word lists
+    load_all_features()
+
+    # get features for training
+    # get_labeled_features()
+
+    # # train various classifiers
+    # for i in range(1, 11):
+    #     accuracies = test_classifiers()
+    #     table = tabulate(accuracies.items(), headers=["Model", "Accuracy"])
+    #     print(table + "\n\n")
+    #
+    #     with open("/Volumes/24265241/Unsupervised Training/model_accuracies.txt", "a") as f:
+    #         f.write(f"Test {i}\n------\n")
+    #         f.write(table + "\n\n")
+    #
+    # # train the best classifiers on whole dataset and store as objects
+    # best_classifiers = {
+    #     "LogisticRegression": LogisticRegression(max_iter=1500),    # avg accuracy: 0.7509538
+    #     "MLPClassifier": MLPClassifier(max_iter=1500),              # avg accuracy: 0.756948
+    #     "AdaBoostClassifier": AdaBoostClassifier(),                 # avg accuracy: 0.752044
+    # }
+    #
+    # for model_name, model in best_classifiers.items():
+    #     classifier = SklearnClassifier(model)
+    #     classifier.train(labeled_dataset_features)
+    #     # save classifier obj for future use
+    #     with open(f"/Volumes/24265241/Classifier Models/{model_name}.pkl", "wb") as f:
+    #         pickle.dump(classifier, f)
+
+    # load in classifiers
+    with open("/Volumes/24265241/Classifier Models/MLPClassifier.pkl", "rb") as f:
+        mlp_classifier: SklearnClassifier = pickle.load(f)
+    with open("/Volumes/24265241/Classifier Models/AdaBoostClassifier.pkl", "rb") as f:
+        ada_boost_classifier: SklearnClassifier = pickle.load(f)
+    with open("/Volumes/24265241/Classifier Models/LogisticRegression.pkl", "rb") as f:
+        logistic_regression_classifier: SklearnClassifier = pickle.load(f)
+
+    # access corpora
+    bbc_corpus = PlaintextCorpusReader("/Volumes/24265241/News Corpus/BBC Corpus", corpora_regex)
+
+    for article in tqdm(bbc_corpus.fileids()):
+        article_features = extract_features(bbc_corpus.raw(article))
+
+        predictions = {
+            "MLPCLassifier": mlp_classifier.classify(article_features),
+            "AdaBoostClassifier": ada_boost_classifier.classify(article_features),
+            "LogisticRegression": logistic_regression_classifier.classify(article_features)
         }
 
-        for model_name, model in tqdm(sklearn_classifiers.items(), desc="Testing Models"):
-            classifier = SklearnClassifier(model)
-            classifier.train(labeled_dataset_features[:train_count])
-            model_accuracies[model_name] = accuracy(classifier, labeled_dataset_features[train_count:])
-
-        print(tabulate(model_accuracies.items(), headers=["Model", "Accuracy"], floatfmt=".2%"))
-
-        with open("/Volumes/24265241/Unsupervised Training/model_accuracies.txt", "a") as f:
-            f.write(f"Test {i}\n------\n")
-            f.write(tabulate(model_accuracies.items(), headers=["Model", "Accuracy"]) + "\n\n")
+        for prediction in predictions.values():
+            if prediction.casefold() == "biased":
+                print(article)
+                exit()
