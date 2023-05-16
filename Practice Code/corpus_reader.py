108db53910f6545a0af18a4f0b0d994cfb8eb7e5
Jack-24265241 <24265241@edgehill.ac.uk>
2023-05-16 12:38:16 +0100

    Removed Excess Files
    Practice code and html pages that were used to aid development have been removed as no longer required.

--- Practice Code/corpus_reader.py
+++
@@ -1,5 +0,0 @@
-from nltk.corpus import PlaintextCorpusReader
-
-corpus_reader = PlaintextCorpusReader("/Volumes/24265241/News Corpus/Independent Corpus", "[a-zA-Z0-9-]*.txt")
-
-print(corpus_reader.fileids())
